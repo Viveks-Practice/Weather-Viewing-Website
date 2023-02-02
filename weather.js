cc3d8acbe20984dab33a959ed13f9ef6e12e02bd
Vivek Sharma <viveksharma@Viveks-MacBook-Pro.local>
2023-02-01 20:54:38 -0800

    created the javascript to make an api call, and extract current weather

---
+++ weather.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+// https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime
+
+export function getWeather(lat, lon, timezone) {
+  return axios
+    .get(
+      "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime",
+      {
+        params: {
+          latitude: lat,
+          longitude: lon,
+          timezone,
+        },
+      }
+    )
+    .then(({ data }) => {
+      return {
+        current: parseCurrentWeather(data),
+        // daily: parseDailyWeather(data),
+        // hourly: parseHourlyWeather(data),
+      };
+    });
+}
+
+function parseCurrentWeather({ current_weather, daily }) {
+  const {
+    temperature: currentTemp,
+    windspeed: windSpeed,
+    weatherCode: iconCode,
+  } = current_weather;
+
+  //this is called destructuring: basically pulling variables/object out of a larger existing object, and renaming them
+  //example:
+  // const maxTemp = daily.temperature_2m_max[0]
+  const {
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+    apparent_temperature_max: [maxFeelsLike],
+    apparent_temperature_min: [minFeelsLike],
+    precipitation_sum: [precip],
+  } = daily;
+
+  return {
+    currentTemp: Math.round(currentTemp),
+    highTemp: Math.round(maxTemp),
+    lowTemp: Math.round(minTemp),
+    highFeelsLike: Math.round(maxFeelsLike),
+    lowFeelsLike: Math.round(minFeelsLike),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+    iconCode,
+  };
+}

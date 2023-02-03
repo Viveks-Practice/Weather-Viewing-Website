752347ad07572620661e0afd5a5417a8c2e550fc
Vivek Sharma <viveksharma@Viveks-MacBook-Pro.local>
2023-02-02 21:59:06 -0800

    completed transforming the data received from the weather api

--- weather.js
+++ weather.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-// https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime
 
 export function getWeather(lat, lon, timezone) {
   return axios
@@ -16,8 +15,8 @@ export function getWeather(lat, lon, timezone) {
     .then(({ data }) => {
       return {
         current: parseCurrentWeather(data),
-        // daily: parseDailyWeather(data),
-        // hourly: parseHourlyWeather(data),
+        daily: parseDailyWeather(data),
+        hourly: parseHourlyWeather(data),
       };
     });
 }
@@ -50,4 +49,28 @@ function parseCurrentWeather({ current_weather, daily }) {
     precip: Math.round(precip * 100) / 100,
     iconCode,
   };
+}
+
+function parseDailyWeather({ daily }) {
+  return daily.time.map((time, index) => {
+    return {
+      timestamp: time * 1000,
+      iconCode: daily.weathercode[index],
+      maxTemp: Math.round(daily.temperature_2m_max[index]),
+    };
+  });
+}
+
+function parseHourlyWeather({ hourly, current_weather }) {
+  return hourly.time
+    .map((time, index) => {
+      return {
+        timestamp: time * 1000,
+        temp: Math.round(hourly.temperature_2m[index]),
+        feelsLike: Math.round(hourly.apparent_temperature[index]),
+        windSpeed: Math.round(hourly.windspeed_10m[index]),
+        precip: Math.round(hourly.precipitation[index] * 100) / 100,
+      };
+    })
+    .filter(({ timestamp }) => timestamp > current_weather.time * 1000);
 }

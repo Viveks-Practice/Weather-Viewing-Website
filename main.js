1219f36d78f97d77695ed2c1e7b6d2180f23c517
Vivek Sharma <viveksharma@Viveks-MacBook-Pro.local>
2023-02-04 12:38:46 -0800

    completed weather app

--- main.js
+++ main.js
@@ -2,16 +2,26 @@ import "./style.css";
 import { getWeather } from "./weather";
 import { ICON_MAP } from "./iconMap";
 
-getWeather(
-  49.162731,
-  -122.784402,
-  Intl.DateTimeFormat().resolvedOptions().timeZone
-)
-  .then(renderWeather)
-  .catch((e) => {
-    console.error(e);
-    alert("Error getting weather.");
-  });
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
+
+function positionSuccess({ coords }) {
+  getWeather(
+    coords.latitude,
+    coords.longitude,
+    Intl.DateTimeFormat().resolvedOptions().timeZone
+  )
+    .then(renderWeather)
+    .catch((e) => {
+      console.error(e);
+      alert("Error getting weather.");
+    });
+}
+
+function positionError() {
+  alert(
+    "There was an error getting your location. Please allow us to use your location and refresh the page"
+  );
+}
 
 function renderWeather({ current, daily, hourly }) {
   renderCurrentWeather(current);

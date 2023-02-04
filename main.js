5d0133a507a46162f7d48c5ee4827ba888f0a968
Vivek Sharma <viveksharma@Viveks-MacBook-Pro.local>
2023-02-04 12:15:01 -0800

    finished rendering data for a static location

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ getWeather(
 
 function renderWeather({ current, daily, hourly }) {
   renderCurrentWeather(current);
-  // renderDailyWeather(daily);
-  // renderHourlyWeather(hourly);
+  renderDailyWeather(daily);
+  renderHourlyWeather(hourly);
   document.body.classList.remove("blurred");
 }
 
@@ -29,7 +29,6 @@ function getIconUrl(iconCode) {
 }
 
 const currentIcon = document.querySelector("[data-current-icon]");
-
 function renderCurrentWeather(current) {
   currentIcon.src = getIconUrl(current.iconCode);
   setValue("current-temp", current.currentTemp);
@@ -39,4 +38,38 @@ function renderCurrentWeather(current) {
   setValue("current-fl-low", current.lowFeelsLike);
   setValue("current-wind", current.windSpeed);
   setValue("current-precip", current.precip);
+}
+
+const DAY_FORMATTER = Intl.DateTimeFormat(undefined, { weekday: "long" });
+const dailySection = document.querySelector("[data-day-section]");
+const dayCardTemplate = document.getElementById("day-card-template");
+function renderDailyWeather(daily) {
+  dailySection.innerHTML = "";
+  daily.forEach((day) => {
+    const element = dayCardTemplate.content.cloneNode(true);
+    setValue("temp", day.maxTemp, { parent: element });
+    setValue("date", DAY_FORMATTER.format(day.timestamp), { parent: element });
+    element.querySelector("[data-icon]").src = getIconUrl(day.iconCode);
+    dailySection.append(element);
+  });
+}
+
+const HOUR_FORMATTER = Intl.DateTimeFormat(undefined, { hour: "numeric" });
+const hourlySection = document.querySelector("[data-hour-section]");
+const hourRowTemplate = document.getElementById("hour-row-template");
+function renderHourlyWeather(hourly) {
+  hourlySection.innerHTML = "";
+  hourly.forEach((hour) => {
+    const element = hourRowTemplate.content.cloneNode(true);
+    setValue("temp", hour.temp, { parent: element });
+    setValue("fl-temp", hour.feelsLike, { parent: element });
+    setValue("wind", hour.windSpeed, { parent: element });
+    setValue("precip", hour.precip, { parent: element });
+    setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element });
+    setValue("time", HOUR_FORMATTER.format(hour.timestamp), {
+      parent: element,
+    });
+    element.querySelector("[data-icon]").src = getIconUrl(hour.iconCode);
+    hourlySection.append(element);
+  });
 }
